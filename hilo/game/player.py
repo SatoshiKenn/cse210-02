f64d6edb77f79b8b1d2a8de6ce15e7f9839767c1
Kennette Guevara <92239496+SatoshiKenn@users.noreply.github.com>
2022-05-05 22:54:17 -0400

    comments

---
+++ hilo/game/player.py
@@ -0,0 +1,43 @@
+class Player:
+    """A person who plays the game. 
+    
+    The responsibility of a player is to choose if the next card will be greater or lower than the dealer's card.
+
+    Attributes:
+        points (int): Player starting score.
+    """
+    points = 300
+
+
+    def __init__(self):
+        """Constructs a new Player.
+        
+        Args:
+            self (Player): an instance of Player.
+        """
+        self.points = 300
+        pass
+
+    def guess():
+        """Ask the player for the number to guess if is greater or lower.
+
+        Args:
+            self (Player): An instance of Player.
+        """
+        pass
+
+    def change_points(self):
+        """Updates the player's score.
+
+        Args:
+            self (Player): An instance of Player.
+        """
+        pass
+
+    def play_again():
+        """Asks the player to play again.
+
+        Args:
+            self (Player): An instance of Player.
+        """
+        pass
